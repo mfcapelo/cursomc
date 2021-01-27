338bc7ba3515c4bde73aeecc4ff27914105172d0
Martinho <mfcapelo65@gmail.com>
2021-01-27 12:42:32 -0300

    Pedido, Pagamento, EstadoPagamento

--- src/main/java/com/celioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/celioalves/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.celioalves.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.celioalves.cursomc.domain.Cidade;
 import com.celioalves.cursomc.domain.Cliente;
 import com.celioalves.cursomc.domain.Endereco;
 import com.celioalves.cursomc.domain.Estado;
+import com.celioalves.cursomc.domain.Pagamento;
+import com.celioalves.cursomc.domain.PagamentoComBoleto;
+import com.celioalves.cursomc.domain.PagamentoComCartao;
+import com.celioalves.cursomc.domain.Pedido;
 import com.celioalves.cursomc.domain.Produto;
+import com.celioalves.cursomc.domain.enums.EstadoPagamento;
 import com.celioalves.cursomc.domain.enums.TipoCliente;
 import com.celioalves.cursomc.repositories.CategoriaRepository;
 import com.celioalves.cursomc.repositories.CidadeRepository;
 import com.celioalves.cursomc.repositories.ClienteRepository;
 import com.celioalves.cursomc.repositories.EnderecoRepository;
 import com.celioalves.cursomc.repositories.EstadoRepository;
+import com.celioalves.cursomc.repositories.PagamentoRepository;
+import com.celioalves.cursomc.repositories.PedidoRepository;
 import com.celioalves.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -85,6 +97,22 @@ public class CursomcApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		
+		ped1.setPagamento(pagto1); 
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 
--- src/main/java/com/celioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/celioalves/cursomc/domain/Cliente.java
@@ -12,7 +12,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.celioalves.cursomc.domain.enums.TipoCliente;
@@ -36,6 +35,9 @@ public class Cliente  implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {}
 
@@ -96,6 +98,24 @@ public class Cliente  implements Serializable {
 		this.enderecos = enderecos;
 	}
 
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -121,13 +141,5 @@ public class Cliente  implements Serializable {
 		return true;
 	}
 
-	public Set<String> getTelefones() {
-		return telefones;
-	}
 
-	public void setTelefones(Set<String> telefones) {
-		this.telefones = telefones;
-	}
-	
-	
 }
